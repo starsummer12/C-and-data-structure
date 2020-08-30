f2b594362115b65145070eec0497be05f039f7f9
yuanrui <yuanrui0912@yeah.net>
2020-08-30 14:59:50 +0800

    initial commit

---
+++ C_language/Narcissus/cmake-build-debug/CMakeFiles/Narcissus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Narcissus.dir/main.c.o"
+  "Narcissus"
+  "Narcissus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Narcissus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ data_structure/linearlist/cmake-build-debug/CMakeFiles/linearlist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linearlist.dir/main.c.o"
+  "linearlist"
+  "linearlist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linearlist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
